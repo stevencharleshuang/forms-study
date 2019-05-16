2a0ee0bf197e9562ce79b243c878ddaec4b6f692
Steven Huang <steven.charles.huang@gmail.com>
2019-05-15 20:57:28 -0400

    added logic for a form reset

--- html/scripts/index.js
+++ html/scripts/index.js
@@ -1,7 +1,36 @@
+// Form HTML String
+const formHTMLStr = `
+  <form action="" method="post">
+    <div class="form-fields">
+      <label for="firstname">
+        First Name:
+      </label>
+      <input class="form-input-field" type="text" name="firstName">
+      <label for="lastname">
+        Last Name:
+      </label>
+      <input class="form-input-field" type="text" name="lastName">
+      <label for="email">
+        E-Mail:
+      </label>
+      <input class="form-input-field" type="email" name="email" required="true">
+      <label for="message">
+        Message:
+      </label>
+      <textarea class="form-input-field" name="body" id="" cols="30" rows="10"></textarea>
+    </div>
+    <button class="btn submit-btn">Submit</button>
+  </form>`;
+
+// DOM Selectors
 const inputFields = document.querySelectorAll('.form-input-field');
 const submitBtn = document.querySelector('.submit-btn');
 const formArea = document.querySelector('.form-area');
 const form = document.querySelector('form');
+let newPostBtn;
+
+// API URLs
+const apiURL = 'http://localhost:4000/api/messages';
 
 let formData = {
   firstName: '',
@@ -23,13 +52,27 @@ const postFormData = (url = '', data = {}) => {
       referrer: 'no-referrer',
       body: JSON.stringify(data)
   })
-  .then(response => response.json()); // parses JSON response into native Javascript objects 
+  .then(response => console.log(response.json())); // parses JSON response into native Javascript objects 
+}
+
+const createNewForm = () => {
+  formArea.innerHTML = formHTMLStr;
+  submitBtn.addEventListener('click', handleSubmit);
+  inputFields.forEach(() => addEventListener('change', handleInput));
 }
 
 const showSuccess = () => {
   const successMsg = document.createElement('h3');
+  newPostBtn = document.createElement('button');
+
   successMsg.innerText = 'Form submission was successful!';
+  newPostBtn.innerText = '"POST" again!';
+  newPostBtn.setAttribute('class', 'btn post-again-btn');
+
   formArea.appendChild(successMsg);
+  formArea.appendChild(newPostBtn);
+
+  newPostBtn.addEventListener('click', createNewForm);
 }
 
 const removeForm = () => {
@@ -38,7 +81,7 @@ const removeForm = () => {
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  postFormData('http://localhost:4000/api/messages', formData);
+  postFormData(apiURL, formData);
   removeForm();
   showSuccess();
 };
